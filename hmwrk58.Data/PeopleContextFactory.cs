a156bd2c2a988af2c4c8744adba4e1cc87b1a400
Bracha Miriam Neuberger <brachamneuberger@gmail.com>
2022-05-10 20:59:51 -0400

    Add project files.

---
+++ hmwrk58.Data/PeopleContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hmwrk58.Data
+{
+    public class PeopleContextFactory
+    {
+        public PeopleDataContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), $"..{Path.DirectorySeparatorChar}hmwrk58.Web"))
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true).Build();
+
+            return new PeopleDataContext(config.GetConnectionString("ConStr"));
+        }
+    }
+}
